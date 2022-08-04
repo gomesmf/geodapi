55fa2c9bb092cd237fda1f3a1480cdbfc5d89eb7
Matheus Gomes <gomes.matheusfelipe@gmail.com>
2022-08-04 16:30:51 -0300

    Add api.py and inmemdb.py

---
+++ api.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+from inmemdb import get_dba
+
+dba = get_dba()
+
+app = FastAPI(description="Delivery Guy API")
+
+@app.get("/accounts")
+def get_accounts():
+    return dba.data
+
+@app.post("/accounts")
+def create_account():
+    return {"account_id": 1}
---
+++ inmemdb.py
@@ -0,0 +1,24 @@
+from typing import Dict
+
+from accounts.interactors import DBAccountsInterface
+
+class InMemoryDBAccounts(DBAccountsInterface):
+    def __init__(self, data: Dict = None) -> None:
+        if data is None:
+            data = {"last_account_id": 0, "accounts": {}}
+
+        self.data = data
+
+    def create(self, atype: str, name: str, email: str, password_hashed: str):
+        self.data["last_account_id"] += 1
+        aid = self.data["last_account_id"]
+        self.data["accounts"][aid] = {
+            "type": atype,
+            "name": name,
+            "email": email,
+            "password_hashed": password_hashed
+        }
+        return aid
+
+def get_dba():
+    return InMemoryDBAccounts()
