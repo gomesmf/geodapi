0f4817324401a0675a5b656a71cd22b76898d114
Matheus Gomes <gomes.matheusfelipe@gmail.com>
2022-08-04 22:11:00 -0300

    Add create account route

--- api.py
+++ api.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
+from accounts.controllers import CreateAccountReqM, create_account_controller
+from accounts.interactors import create_account_interactor
+from accounts.presenters import create_account_presenter
+from accounts.views import create_account_view
 
-from fake import get_dba
+from fake import get_dba, fake_password_hash
 
 dba = get_dba()
+
+ph = fake_password_hash
 
 app = FastAPI(description="Delivery Guy API")
 
@@ -11,5 +17,9 @@ def get_accounts():
     return dba.data
 
 @app.post("/accounts")
-def create_account():
-    return {"account_id": 1}
+def create_account(reqm: CreateAccountReqM):
+    ucin = create_account_controller(reqm)
+    ucout = create_account_interactor(dba, ph, ucin)
+    vm = create_account_presenter(ucout)
+    resm = create_account_view(vm)
+    return resm
