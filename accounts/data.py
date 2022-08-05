a1996767f95502856321da34b005ddf2c44d64b1
Matheus Gomes <gomes.matheusfelipe@gmail.com>
2022-08-05 20:43:48 -0300

    Update account database interface

--- accounts/data.py
+++ accounts/data.py
@@ -5,9 +5,13 @@ from accounts.entities import Account
 
 class DBAccountsInterface(ABC):
     @abstractmethod
-    def create(self, a: Account) -> int:
+    def create(self, a: Account) -> bool:
         pass
 
     @abstractmethod
     def email_exists(self, email: str) -> bool:
+        pass
+
+    @abstractmethod
+    def delete(self, account_id: int) -> bool:
         pass
