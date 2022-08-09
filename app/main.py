6c890a05278a2aff0685a2cb77953151b5c9b529
Matheus Gomes <gomes.matheusfelipe@gmail.com>
2022-08-09 11:19:37 -0300

    Update app description

--- app/main.py
+++ app/main.py
@@ -6,7 +6,14 @@ from app.routers import accounts, distances
 
 from accounts.auth import authenticate_account, create_account_id_access_token
 
-app = FastAPI(description="Compute Geodesic Distance Between Address API")
+description = [
+    "Geodesic Distance API",
+    "1. Create account (POST /accounts) and login (button 'Authorize')",
+    "2. Compute geodesic distance between two addresses (POST /distances)",
+    "3. Get historical queries (GET /distances)"
+]
+
+app = FastAPI(description="\n".join(description))
 
 app.include_router(accounts.router)
 app.include_router(distances.router)
